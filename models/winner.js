dafdd609c969c911a6bfdfb762ac2bedecbfe957
tiago-py <tiago.braga0012@gmail.com>
2024-10-24 11:38:32 -0300

    [fix]Corrigindo tipagem do numero da sorte

--- models/winner.js
+++ models/winner.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const WinnerSchema = new mongoose.Schema({
     name: { type: String, required: true }, // Nome do ganhador (obrigatório)
     cpf: { type: String, required: true }, // Nome do ganhador (obrigatório)
-    lucky_number: { type: String, required: true }, // Nome do ganhador (obrigatório)
+    lucky_number: { type: Number, required: true }, // Nome do ganhador (obrigatório)
     prize: { type: String, required: true }, // Prêmio do ganhador (obrigatório)
     date: { type: Date, required: true } // Data do prêmio (obrigatório)
 });
